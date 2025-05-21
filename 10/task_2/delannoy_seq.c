7f09c6e5a5748b10649ab75164b8ce0f57d3c078
lucauibk <150453615+lucauibk@users.noreply.github.com>
2025-05-21 13:21:41 +0200

    ex10

---
+++ 10/task_2/delannoy_seq.c
@@ -0,0 +1,28 @@
+//D(m, n) = 1                          if m = 0 or n = 0
+//D(m-1, n) + D(m-1, n-1) + D(m, n-1)   otherwise
+
+#include <stdio.h>
+#include <stdlib.h>
+
+unsigned long long delannoy(int m, int n) {
+    if(m == 0 || n == 0){
+        return 1;
+    }else{
+        return delannoy(m-1, n) + delannoy(m-1, n-1) + delannoy(m, n-1);
+    }
+}
+
+int main(int argc, char* argv[]){
+    if(argc != 2){
+        printf("Usage: %s <n>\n", argv[0]);
+        return 1;
+    }
+    int N = atoi(argv[1]);
+    double start = omp_get_wtime();
+    unsigned long long result = delannoy(N, N);
+    double end = omp_get_wtime();
+
+    printf("Delannoy(%d, %d) = %llu\n", N, N, result);
+    printf("Time: %.6f seconds\n", end - start);
+    return 0;
+}
